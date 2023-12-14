b8c68242aac0f1dc53a19a760af30d47a1217cdf
Architsharma7 <architsharma7911@gmail.com>
2023-12-14 23:45:26 +0530

    script added for deployment

--- fundamentals/script/Counter.s.sol
+++ fundamentals/script/Counter.s.sol
@@ -2,11 +2,16 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
-    function setUp() public {}
+    // function setUp() public {}
 
-    function run() public {
+    function run() external returns (Counter) {
         vm.broadcast();
+        // every transaction should be inside broadcast and stopbroadcast, define your vairables and constants before broadcast
+        Counter counter = new Counter();
+        // vm.stopBroadcast();
+        return counter;
     }
 }
